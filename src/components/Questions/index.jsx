8dfd8ab59d8ee03ec68633e317bba10b511cdd03
vivian <vivian.v18723@gmail.com>
2022-09-04 20:01:48 +0100

    create & export questions component

---
+++ src/components/Questions/index.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Questions() {
+  return (
+    <>
+      <h1>Question 1/5</h1>
+      <p>What is the largest country in Asia?</p>
+      <button>Singapore</button>
+      <button>China</button>
+      <button>Thailand</button>
+      <button>Japan</button>
+    </>
+  );
+}
+
+export default Questions;
