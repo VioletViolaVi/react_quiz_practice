e4aa353b5d68ca8d224210e2e0d6f63236dac6c5
vivian <vivian.v18723@gmail.com>
2022-09-04 23:42:52 +0100

    add more useState variables attempt to pass props to scoreboard components attempt to dynamically change score

--- src/components/QuizGameHome/index.jsx
+++ src/components/QuizGameHome/index.jsx
@@ -5,25 +5,41 @@ import Scoreboard from "../Scoreboard";
 import questionArr from "../Data";
 
 function QuizGameHome() {
-  /* number for changing questions & options */
+  /* incrementing number for changing questions & options */
   const [increment, setIncrement] = useState(0);
-  /* toggles appearance of scoreboard */
-  const [showScore, setShowScore] = useState(false);
 
-  /* increases number to change question & options */
-  const handleMoveToNextQuestion = () => {
+  /* toggles appearance of scoreboard @ end of game */
+  const [showResults, setShowResults] = useState(false);
+
+  /* updates player's score */
+  const [playerScore, setPlayerScore] = useState(0);
+
+  const handleMoveToNextQuestion = (isRightParam) => {
+    console.log("isRightParam: ", isRightParam);
+
+
+    /* keeps track of user's score */
+    if (isRightParam === true) {
+      console.log("This is true, CORRECT!!!");
+      setPlayerScore((prev) => {
+        prev += 1;
+        return prev;
+      });
+    }
+
+    /* increases incrementing number to change question & options */
     let newQuestion = increment + 1;
 
     if (newQuestion < questionArr.length) {
       setIncrement((prev) => {
         prev = newQuestion;
-        console.log("newQuestion: ", newQuestion, "questionArr.length: ", questionArr.length);
         return prev;
       });
-    } 
-    
+    }
+
+    /* shows opposite boolean to display/hide scores */
     if (newQuestion === questionArr.length) {
-      setShowScore((prev) => {
+      setShowResults((prev) => {
         return !prev;
       });
     }
@@ -31,8 +47,8 @@ function QuizGameHome() {
 
   return (
     <>
-      {showScore ? (
-        <Scoreboard />
+      {showResults ? (
+        <Scoreboard arrLength={questionArr.length} score={playerScore} />
       ) : (
         <Questions
           increment={increment}
