c9ad71774a557205f9b24f4428aff3fe81111341
vivian <vivian.v18723@gmail.com>
2022-09-04 20:53:11 +0100

    update data with ids & another object

--- src/components/Data/index.jsx
+++ src/components/Data/index.jsx
@@ -1,54 +1,64 @@
-const questionObjArr = [
+const questionArr = [
   {
     id: 1,
     question: "What colour added to yellow makes green?",
     optionsArr: [
-      { option: "brown", isRight: false },
-      { option: "orange", isRight: false },
-      { option: "red", isRight: false },
-      { option: "blue", isRight: true }
+      { id: 1, option: "brown", isRight: false },
+      { id: 2, option: "orange", isRight: false },
+      { id: 3, option: "red", isRight: false },
+      { id: 4, option: "blue", isRight: true }
     ]
   },
   {
     id: 2,
     question: "What is the largest country in Asia?",
     optionsArr: [
-      { option: "Singapore", isRight: false },
-      { option: "China", isRight: true },
-      { option: "Thailand", isRight: false },
-      { option: "Japan", isRight: false }
+      { id: 1, option: "Singapore", isRight: false },
+      { id: 2, option: "China", isRight: true },
+      { id: 3, option: "Thailand", isRight: false },
+      { id: 4, option: "Japan", isRight: false }
     ]
   },
   {
     id: 3,
     question: "How many eggs are in a dozen?",
     optionsArr: [
-      { option: 34, isRight: false },
-      { option: 7, isRight: false },
-      { option: 12, isRight: true },
-      { option: 65, isRight: false }
+      { id: 1, option: "34", isRight: false },
+      { id: 2, option: "7", isRight: false },
+      { id: 3, option: "12", isRight: true },
+      { id: 4, option: "65", isRight: false }
     ]
   },
   {
     id: 4,
     question: "Which is the largest planet in the solar system?",
     optionsArr: [
-      { option: "Jupiter", isRight: true },
-      { option: "Pluto", isRight: false },
-      { option: "Earth", isRight: false },
-      { option: "Mars", isRight: false }
+      { id: 1, option: "Jupiter", isRight: true },
+      { id: 2, option: "Pluto", isRight: false },
+      { id: 3, option: "Earth", isRight: false },
+      { id: 4, option: "Mars", isRight: false }
     ]
   },
   {
     id: 5,
     question: "What is the square root of 676?",
     optionsArr: [
-      { option: 26, isRight: true },
-      { option: 24, isRight: false },
-      { option: 22, isRight: false },
-      { option: 20, isRight: false }
+      { id: 1, option: "26", isRight: true },
+      { id: 2, option: "24", isRight: false },
+      { id: 3, option: "22", isRight: false },
+      { id: 4, option: "20", isRight: false }
+    ]
+  },
+  {
+    id: 6,
+    question: "Which sign of the zodiac comes between Virgo and Scorpio?",
+    optionsArr: [
+      { id: 1, option: "Gemini", isRight: false },
+      { id: 2, option: "Taurus", isRight: false },
+      { id: 3, option: "Libra", isRight: true },
+      { id: 4, option: "Capricorn", isRight: false }
     ]
   }
 ];
 
-export default questionObjArr;
+export default questionArr;
