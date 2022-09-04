9daaab5e88b96ee0e5255cab1e8938e8704603d2
vivian <vivian.v18723@gmail.com>
2022-09-04 20:53:42 +0100

    use useState to change questions

--- src/components/Questions/index.jsx
+++ src/components/Questions/index.jsx
@@ -1,14 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
+
+import questionArr from "../Data";
 
 function Questions() {
+  // number for changing questions & options
+  const [increment, setIncrement] = useState(0);
+
+  // increases number to change question & options
+  const handleMoveToNextQuestion = () => {
+    let newQuestion = increment + 1;
+
+    if (newQuestion < questionArr.length) {
+      setIncrement((prev) => {
+        prev = newQuestion;
+        return prev;
+      });
+    } else {
+      alert("End of quiz");
+    }
+  };
+
   return (
     <>
-      <h1>Question 1/5</h1>
-      <p>What is the largest country in Asia?</p>
-      <button>Singapore</button>
-      <button>China</button>
-      <button>Thailand</button>
-      <button>Japan</button>
+      <h1>
+        Question {increment + 1}/{questionArr.length}
+      </h1>
+      <p> {questionArr[increment].question}</p>
+
+      {questionArr[increment].optionsArr.map((singleOptionObj) => {
+        return (
+          <button onClick={handleMoveToNextQuestion} key={singleOptionObj.id}>
+            {singleOptionObj.option}
+          </button>
+        );
+      })}
     </>
   );
 }
